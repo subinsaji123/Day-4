f7c54ca6ce6ad51987884685576c482069ccda17
subinsaji123 <subinsaji2001@gmail.com>
2023-12-15 14:40:39 +0530

    hello

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { DistrictComponent } from './cards/district/district.component';
+import { SingleComponent } from './pages/single/single.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',component:HomeComponent
+    },
+    {
+        path:'contact',component:ContactComponent
+    },
+    {
+        path:'district',component:DistrictComponent
+    },
+    {
+        path:'district/:id',component:SingleComponent
+    }
+];
